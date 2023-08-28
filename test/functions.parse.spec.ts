661ada13a8dc0f671280c897814e75d0b2d95bce
Pranciskus Ambrazas <p.ambrazas@gmail.com>
2023-08-28 09:16:02 +0300

    eslint fixes + two expects

--- test/functions.parse.spec.ts
+++ test/functions.parse.spec.ts
@@ -19,5 +19,7 @@ describe('parsing', () => {
   it('parses geometry collection', () => {
     const result: FeatureCollection = parse(geometryCollection);
     expect(result.features.length).toEqual(2);
+    expect(result.features[0].geometry.coordinates).toEqual(geometryCollection.geometries[0].coordinates);
+    expect(result.features[1].geometry.coordinates).toEqual(geometryCollection.geometries[1].coordinates);
   });
 });
